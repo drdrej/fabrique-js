2379c78529d0b619166a8170be53b24685fab363
Andreas Siebert <andrej.siebert@gmail.com>
2013-11-14 22:47:46 +0100

    working on cli

--- impl/cli.js
+++ impl/cli.js
@@ -5,18 +5,22 @@ var startShell = function() {
     var readline = require('readline');
 
     var input = readline.createInterface(process.stdin, process.stdout);
+
+    //while(true) {
     input.setPrompt('fabrique> ');
     input.prompt();
 
     input.on( 'line', function(line) {
-        if (line === "exit")
-            input.close();
-
+        var command = parseLine(line);
+        input.setPrompt('fabrique> ');
         input.prompt();
+
+        return;
     }).on('close',function(){
             // that.closeFabrique();
             process.exit(0);
         });
+    // }
 };
 
 
@@ -28,6 +32,40 @@ var env = createEnv( rootDir );
 
 
 startShell();
+
+
+
+var parseLine = function(line) {
+    var trimmed = "";
+
+    if( !line
+        || !_.isString(line)
+        || ((trimmed = line.trim()).length < 1) ) {
+        console.log( pad("nothing to execute. borring.") );
+
+        return;
+    }
+
+    var splitted = trimmed.split(" ");
+
+    if( !splitted || !_.isArray(splitted) ) {
+        console.log( pad("couldn't parse cli. retry!") );
+
+        return;
+    }
+
+    // init command to start execution :::
+    var cmd = {
+        name : splitted[0]
+    };
+
+    console.log(pad("try to exec command: " + cmd.name ));
+};
+
+var pad = function( msg ) {
+    var S = require('string');
+    return S(msg).padLeft((msg.length + 5)).toString();
+};
 
 
 
