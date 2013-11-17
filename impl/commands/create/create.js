25fa00d8e9bb846f6a27b21f2db042aad7761915
Andreas Siebert <andrej.siebert@gmail.com>
2013-11-17 13:09:10 +0100

    switch to gpl v3

--- impl/commands/create/create.js
+++ impl/commands/create/create.js
@@ -1,15 +1,26 @@
 
-exports.exec = function() {
+var Create = function() {
+    // TODO: create muss den generator auflösen und ausführen.
+    // vorher backbone initialisieren.
+};
+
+
+exports.exec = function( ) {
   //console.log("#################################################-----");
   //console.log( this.def );
   //console.log("#################################################-----");
+
+  // generator auflösen
+  // Backbone initialisieren
+  // Backbone.route finden/feuern
 };
 
+
 exports.args = [{
-    name :  "resource",
+    name : "resource",
     type : "STRING",
     short : "r",
     handler : function( option, value ) {
-         console.log( ">>>>>>>>>>> rockn'rollll");
+         // console.log( ">>>>>>>>>>> rockn'rollll");
     }
 }];
